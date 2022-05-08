223167d127639adeeb958dccc7f65cc758108876
梁瑶瑶 <2623255939@qq.com>
2022-05-08 12:57:31 +0800

    U init

--- xianxia-springtest/src/main/java/com/xianxia/springs/test/MySpringsTest.java
+++ xianxia-springtest/src/main/java/com/xianxia/springs/test/MySpringsTest.java
@@ -1,11 +1,12 @@
 package com.xianxia.springs.test;
 
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MySpringsTest {
 
 	public static void main(String[] args) {
-//		new Class
+		new ClassPathXmlApplicationContext("");
 		System.err.println("tes");
 
 	}
